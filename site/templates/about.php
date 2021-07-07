75d116d414ec51eea6bc3f1b52389dcc4b132ddd
Tereza <tereznovak@email.cz>
2021-07-07 20:47:09 +0200

    about

--- site/templates/about.php
+++ site/templates/about.php
@@ -13,9 +13,9 @@
 <?php snippet('header-about') ?>
 
 <article class="about-us">
-      <p>Dear Reader, </br></br>
+      <p>Dear Visitor, </br></br>
 
-          We hope you are well.</br></br>
+          Welcome to our exhibition, From Where We Know. </br></br>
 
           We are <a href="<?= $pages->find('artist')->url() ?>">37 students</a> from the department of Graphic Design (BA) and <a href="<?= $pages->find('artist')->url() ?>">14 students</a> from the department Non Linear Narrative (MA) at the Royal Academy of Art, The Hague (KABK). We know you are curious about what we have been up to, maybe you're even wondering where we have been all this time.</br></br>
 
@@ -28,23 +28,165 @@
           <div class="popup" onclick="myFunction()"><u>where it is that we know from. (1)</u>
             <span class="popuptext" id="myPopup"><p>(1) The title of our exhibition draws from the essay <a href="https://zoestodd.com/2021/01/19/weaponized-fossil-kin-and-the-alberta-economy/#_ftn4">Weaponized Fossil Kin and the Alberta economy</a> by Dr. Zoe Todd, in which she recounts an instructive teaching of geographer Katherine McKittrick, which is to ask: &ldquo;Where do you know from?"</p>
             </span>
-          </div> Through this exhibition you can encounter our works in a new constellation of geographies, organised around sites of research and 
-          <div class="popup-1" onclick="myFunction1()"><u>taxonomies that connect us. (2)</u>
-            <span class="popuptext-1" id="myPopup-1"><p>(2) The taxonomy of keywords used in our exhibition and online platform was developed by Dr. Adeola Enigbokan, who was the external examiner of the Graphic Design (BA) graduation in 2020-21. The framing emerged based on our exchanges with Dr. Enigbokan, and captures an overview of the concerns and interests we share as a class. Dr. Enigbokan made this framing after having read our theses, and we extended the keywords to the Non Linear Narrative (MA) department.</span>
+          </div> You can re-visit our works on the platform from-where-we-know.net where you can encounter them in a new constellation of geographies, organised around sites of research and <div class="popup-1" onclick="myFunction1()"><u>taxonomies (2)</u> that connect us.
+            <span class="popuptext-1" id="myPopup-1"><p>(2) The taxonomy of keywords used on our platform was developed with Dr. Adeola Enigbokan, who was the external examiner of the Graphic Design (BA) graduation in 2020-21. The framing captures an overview of the concerns and interests we share as a class, and we extended the keywords to the Non Linear Narrative (MA) department.</span>
           </div>
-          These whereabouts and stances we want to share at our upcoming graduation show, From Where We Know, which will take place during the Graduation Festival 2021 at The Royal Academy of Art, The Hague (July 8th - 11th).</br></br>
-
-          On this page are gathered the emerging taxonomies, collections, and overviews of our landscape of processes, we invite you to keep an eye on it in the upcoming weeks as it grows!
-
-          You are also kindly invited to donate to our fundraising campaign at <a href="https://www.voordekunst.nl/projecten/12295-from-where-we-know-1">Voordekunst</a>  – your contribution is immensely appreciated.</br></br>
-
-          We hope to meet you there, and in the meantime, please do stay in touch.</br></br>
+          We hope we can get to know you, and that we can stay in touch.</br></br>
 
           With Love,</br></br>
 
           Students of <a href="https://www.kabk.nl/en/programmes/bachelor/graphic-design?mark=graphic%20design">Graphic Design (BA)</a> and <a href="https://www.kabk.nl/en/programmes/master/non-linear-narrative?mark=non%20linear">Non Linear Narrative (MA),</a></br>
-          Royal Academy of Art, The Hague (KABK)</p>
+          Royal Academy of Art, The Hague (KABK)</br></br>
           
+          <p1>     
+———————————————</br></br>
+
+From Where We Know</br></br>
+
+<div class="inner-wrapper">
+
+<div class="left">
+
+Research and Concept: </br>Daniela Damaschin, </br>Tuana İnhan, </br>Merel Meijer, </br>Hattie Wade</br></br>
+Graphic Design: </br>Yana Abrasheva, </br>Radek Górniak</br></br>
+Exhibition: </br>Ocean Albin, </br>Rafael Colmanett, </br>Dominik Vrabič Dežman, </br>Tuana İnhan, </br>MD2 (Mahaut Dael, </br>Michał Długajczyk, </br>Daria Kozhina), </br>Merel Meijer, </br>Tereza Nováková, </br>Amanda Verberne</br></br>
+Website: </br>Daniela Damaschin, </br>Dominik Vrabič Dežman, </br>Tereza Nováková, </br>Pablo Perez, </br>Amanda Verberne</br></br>
+</div>  
+<div class="right">
+Video Campaign: </br>Daniela Damaschin, </br>Tuana İnhan, </br>Merel Meijer, </br>Hattie Wade</br></br>
+Editing: </br>Dominik Vrabič Dežman, </br>Kexin Hao, </br>Agata Jaworska, </br>Ada Popowicz, </br>Olga Elliot Schou, </br>Hattie Wade</br></br>
+Social Media and Communication: </br>Agata Markowska</br></br>
+Class Photography: </br>Mika Schalks</br></br>
+Guidance: </br>Ina Hollmann, </br>Agata Jaworska</br></br>
+Typefaces: </br>Contrast by Ocean Albin</br>Crouwd by Ralph Bruens</br></br>
+</div> 
+</div> 
+———————————————</br></br>
+
+<div class="inner-wrapper">
+
+<div class="left">
+
+Graphic Design (BA)</br></br>
+
+Iana Abrasheva</br>
+Ocean Albin</br>
+Sacha van Alfen</br>
+Eunjin An</br>
+Ivi Apostolatos </br>
+Emma Benozzi</br>
+Fabiola van den Berg</br>
+Ralph Bruens</br>
+Rafael Colmanett </br>
+Daniela Damaschin</br>
+Dominik Vrabič Dežman</br>
+Linea Lan Cai Fabricius	</br>
+Isabella Giacomelli</br>
+Radek Górniak</br>
+Kexin Hao</br>
+Eva-Maria Horvath</br>
+Michelle Janssen</br>
+Handi Kim</br>
+Agata Markowska </br>
+Merel Meijer</br>
+Tereza Nováková</br>
+Soyun Park </br>
+Karoliina Pärnänen</br>
+Ada Popowicz</br>
+Vincent Rheinberger</br>
+Sjors Rigters</br>
+Kilia Robustella </br>
+Malik Saïb-Mezghiche </br>
+Mika Schalks</br>
+Olga Elliot Schou</br>
+Anke Sondi Rumohr</br>
+Kristiāna Marija Sproģe </br>
+Ian Scheufler</br>
+Aliz Soós </br>
+Amanda Verberne </br>
+Adriana Navarro Villacampa</br>
+Aurora Navarro Villacampa </br></br>
+
+Department Head:</br>
+Roosje Klap</br></br>
+
+Thesis Tutors: </br>
+Merel Boers</br>
+Füsun Türetken</br>
+Dirk Vis </br></br>
+
+Tutors: </br>
+Lauren Alexander</br>
+Thomas Buxó</br>
+Ina Hollmann</br>
+Agata Jaworska </br>
+Roosje Klap </br>
+Silvio Lorusso </br>
+Babusi Nyoni</br>
+Ruben Pater </br>
+Klasien van de Zandschulp </br></br>
+
+External Examiner:</br>
+Adeola Enigbokan</br></br>
+
+Coordinators: </br>
+Ingrid Grunwald </br>
+Marit van der Meulen</br></br>
+</div>  
+<div class="right">
+
+Non Linear Narrative (MA)</br></br>
+
+Katie Pelikan Baselj </br>
+Justine Corrijn</br>
+Sophie Czich</br>
+Esther van der Heijden  </br>
+Tuana İnhan </br>
+Lance Gapuz Laoyan</br>
+Felix Meermann</br>
+Marcin Liminowicz</br>
+Dario di Paolantonio</br>
+Pablo Perez </br>
+Taya Reshetnik</br>
+Natalia Śliwińska  </br>
+Elinor Salomon </br>
+Hattie Wade </br></br>
+
+Department Co-Heads:</br>
+Roosje Klap</br>
+Niels Schrader </br></br>
+
+Thesis Tutors:</br>
+Federico Campagna</br>
+Mijke van der Drift </br></br>
+
+Tutors:</br>
+Lauren Alexander</br>
+Linda van Deursen</br>
+Roosje Klap </br>
+Lizzie Malcolm</br>
+Ruben Pater</br>
+Daniel Powers </br>
+Niels Schrader</br></br>
+
+External Examiner:</br>
+Annet Dekker</br></br>
+
+Coordinators:</br>
+Ingrid Grunwald </br>
+Marit van der Meulen</br></br>
+</div>  
+</div>  
+
+
+
+
+———————————————</br></br>
+
+8-11 July</br>
+Graduation Show 2021</br>
+The Royal Academy of Art, The Hague</p>
+</p1>
     </article>
 
     <script>
